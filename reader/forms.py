4344e762d4f9a77243c0852af984b6a4887c0060
Igor Sharapov <Mr1Penguin@gmail.com>
2015-09-22 13:21:40 +0300

    remove placeholders in bootstrap3

--- reader/forms.py
+++ reader/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class LoginForm(forms.Form):
-	your_email = forms.EmailField(label="Email",widget=forms.TextInput(attrs={'placeholder': ''}))
-	password = forms.CharField(widget=forms.PasswordInput,widget=forms.TextInput(attrs={'placeholder': ''}))
+	your_email = forms.EmailField(label="Email")
+	password = forms.CharField(widget=forms.PasswordInput)
 	widgets = {
             'password': forms.PasswordInput(),
     }
