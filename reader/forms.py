bcb9260b78f50ad00610938b7c20513d7c3a2c3e
Igor Sharapov <Mr1Penguin@gmail.com>
2015-09-22 13:15:32 +0300

    remove placeholders

--- reader/forms.py
+++ reader/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class LoginForm(forms.Form):
-	your_email = forms.EmailField(label="Email")
-	password = forms.CharField(widget=forms.PasswordInput)
+	your_email = forms.EmailField(label="Email",widget=forms.TextInput(attrs={'placeholder': ''}))
+	password = forms.CharField(widget=forms.PasswordInput,widget=forms.TextInput(attrs={'placeholder': ''}))
 	widgets = {
             'password': forms.PasswordInput(),
     }
