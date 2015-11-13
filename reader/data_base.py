2cb869f9998bb1354e92051b1b4503556f887ba2
naglijlamer <NL40.000@mail.ru>
2015-11-13 18:27:18 +0300

    Login page backend

---
+++ reader/data_base.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+conn = 0
+curr = 0
+
+def activate_base():
+	try :
+		global conn
+		global curr
+		conn = psycopg2.connect("dbname='mnreader' user='mpreader' password='MPreader'")
+		curr = conn.cursor()
+	except Exception, e:
+		print "Igor is mooooodaq. Well played."
+		psycopg2.errorcodes.lookup(e.pgcode)
