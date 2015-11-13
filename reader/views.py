ec30dac1839c004f99fd6ecaf26266ffc087da9b
Igor Sharapov <Mr1Penguin@gmail.com>
2015-11-13 18:21:37 +0300

    collection page example

--- reader/views.py
+++ reader/views.py
@@ -5,10 +5,10 @@ from .forms import LoginForm
 
 # Create your views here.
 def index(request):
-    form = LoginForm()
-    if request.POST.get('your_email', '') == '':
+    #form = LoginForm()
+    '''if request.POST.get('your_email', '') == '':
         ren = render(request, 'index.html', {"form": form})
     else:
-        # if correct load collection. else index with error
-        ren = render(request, 'collection.html')
+        # if correct load collection. else index with error'''
+    ren = render(request, 'collection.html')
     return ren
