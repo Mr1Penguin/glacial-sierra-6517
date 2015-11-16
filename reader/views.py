7bdc6be361a46d864a6e5c3864801f59c4b12ad9
naglijlamer <NL40.000@mail.ru>
2015-11-17 00:29:30 +0300

    Some csrf-token features

--- reader/views.py
+++ reader/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.middleware.csrf import get_token
+from django.middleware.csrf import rotate_token
 
 from .forms import LoginForm
 from .data_base import *
@@ -13,8 +15,10 @@ def index(request):
         if request.POST.get('new_user'):
             curr.execute("select user_email from reader_user where user_email=(%s)", (request.POST.get('your_email'),))
             if curr.rowcount == 0:
-                curr.execute("insert into reader_user (user_email, password) values (%s, %s)", (request.POST.get('your_email', 'FUCK'), request.POST.get('password', 'FUCK')))
+                rotate_token(request)
+                curr.execute("insert into reader_user (user_email, password, token) values (%s, %s, %s)", (request.POST.get('your_email', 'FUCK'), request.POST.get('password', 'FUCK'), get_token(request)))
                 conn.commit()
+                ren = render(request, 'collection.html')
             else:
                 content.update({"error": "User with this e-mail already exist!", "lastmail": request.POST.get('your_email'), "checkbox" : True})
         else:
@@ -23,11 +27,19 @@ def index(request):
             if curr.rowcount == 1:
                 passwd = curr.fetchone()[0]
                 if passwd == request.POST.get('password'):
+                    rotate_token(request)
+                    curr.execute("update reader_user set token=(%s) where user_email=(%s)", (get_token(request), request.POST.get('your_email', 'FUCK')))
+                    conn.commit()
                     ren = render(request, 'collection.html')
                 else:
                     content.update({"error": "Wrong password!", "lastmail":request.POST.get('your_email')})
             else:
                 content.update({"error": "User with this e-mail doesn't exist!", "lastmail":request.POST.get('your_email')})
+    else:
+        if request.method == "GET":
+            curr.execute("select user_email from reader_user where token=(%s)", (get_token(request),))
+            if curr.rowcount == 1:
+                ren = render(request, 'collection.html')
     if ren is None:
         ren = render(request, 'index.html', content)
     return ren
