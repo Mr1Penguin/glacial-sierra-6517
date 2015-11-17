effdd5837c9889d1a123704d31463f1ea1085151
Igor Sharapov <IgorSharapov@outlook.com>
2015-11-17 11:41:27 +0300

    models.py indent

--- reader/models.py
+++ reader/models.py
@@ -3,22 +3,22 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-        user_email = models.EmailField()
-        password = models.CharField(max_length=255)
-        token = models.CharField(max_length=255, null=True)
-        def __unicode__ (self):
-                return self.user_email
+    user_email = models.EmailField()
+    password = models.CharField(max_length=255)
+    token = models.CharField(max_length=255, null=True)
+    def __unicode__ (self):
+        return self.user_email
 
 class Site(models.Model):
-        url = models.URLField()
-        add_date = models.DateTimeField()
-        user = models.ForeignKey(User)
+    url = models.URLField()
+    add_date = models.DateTimeField()
+    user = models.ForeignKey(User)
 
 class Image(models.Model):
-        url = models.URLField()
-        site = models.ForeignKey(Site)
-        add_date = models.DateTimeField()
+    url = models.URLField()
+    site = models.ForeignKey(Site)
+    add_date = models.DateTimeField()
 
 #check add date string if none exists
 class Collect(models.Model):
-        collect_date = models.DateTimeField()
+    collect_date = models.DateTimeField()
