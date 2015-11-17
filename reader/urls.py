9ca7c579e6864e4c155c14e56be0522b139c5fd1
Igor Sharapov <IgorSharapov@outlook.com>
2015-11-17 15:09:58 +0300

    load images for site

--- reader/urls.py
+++ reader/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^collection/$', views.collection, name='collection'),
+    url(r'^collection/load_site/$', views.load_site, name='load_site')
 ]
