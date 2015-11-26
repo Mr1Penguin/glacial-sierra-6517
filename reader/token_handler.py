7019b6d1b217042bc76f6690867bed191c83333d
Igor Sharapov <IgorSharapov@outlook.com>
2015-11-26 17:08:31 +0300

    token deleter v1.0

--- reader/token_handler.py
+++ reader/token_handler.py
@@ -1,8 +1,5 @@
-from reader import activate_base
-from reader import curr
-from reader import conn
+from .data_base import *
 
-#def delete_old_tokens():
 activate_base()
 curr.execute("delete from reader_user_token where clock_timestamp()-last_use > interval '1' day")
 conn.commit()
